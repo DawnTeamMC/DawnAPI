0c7a69478fe18115ccbd5ad79c08970ace9a9186
Hugman <contact.hugman@gmail.com>
2020-07-30 14:05:56 +0200

    Added creator for block entities

---
+++ src/main/java/com/hugman/dawn/api/creator/BlockEntityCreator.java
@@ -0,0 +1,45 @@
+package com.hugman.dawn.api.creator;
+
+import com.hugman.dawn.api.util.SimpleBuilder;
+import com.mojang.datafixers.types.Type;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.datafixer.TypeReferences;
+import net.minecraft.util.Util;
+import net.minecraft.util.registry.Registry;
+
+public class BlockEntityCreator extends Creator<BlockEntityType<? extends BlockEntity>> {
+	protected final BlockEntityType.Builder<? extends BlockEntity> builder;
+
+	private BlockEntityCreator(String name, BlockEntityType.Builder<? extends BlockEntity> builder) {
+		super(name);
+		this.builder = builder;
+	}
+
+	@Override
+	public BlockEntityType<? extends BlockEntity> register(ModData modData) {
+		Type<?> type = Util.getChoiceType(TypeReferences.BLOCK_ENTITY, name);
+		value = Registry.register(Registry.BLOCK_ENTITY_TYPE, modData.id(name), builder.build(type));
+		return value;
+	}
+
+	public static class Builder implements SimpleBuilder<BlockEntityCreator> {
+		protected final String name;
+		protected final BlockEntityType.Builder<? extends BlockEntity> builder;
+
+		/**
+		 * Creates a block entity type.
+		 *
+		 * @param name    The name of the block entity.
+		 * @param builder The block entity type builder.
+		 */
+		public Builder(String name, BlockEntityType.Builder<? extends BlockEntity> builder) {
+			this.name = name;
+			this.builder = builder;
+		}
+
+		public BlockEntityCreator build() {
+			return new BlockEntityCreator(this.name, this.builder);
+		}
+	}
+}
