2109cc23bcba78534c54e22631ae43b4d68458ea
Hugman <contact.hugman@gmail.com>
2020-07-30 14:07:25 +0200

    Added creator for effects

---
+++ src/main/java/com/hugman/dawn/api/creator/EffectCreator.java
@@ -0,0 +1,40 @@
+package com.hugman.dawn.api.creator;
+
+import com.hugman.dawn.api.util.SimpleBuilder;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.util.registry.Registry;
+
+public class EffectCreator extends Creator<StatusEffect> {
+	protected final StatusEffect baseEffect;
+
+	private EffectCreator(String name, StatusEffect baseEffect) {
+		super(name);
+		this.baseEffect = baseEffect;
+	}
+
+	@Override
+	public StatusEffect register(ModData modData) {
+		value = Registry.register(Registry.STATUS_EFFECT, modData.id(name), baseEffect);
+		return value;
+	}
+
+	public static class Builder implements SimpleBuilder {
+		protected final String name;
+		protected final StatusEffect baseEffect;
+
+		/**
+		 * Creates an effect.
+		 *
+		 * @param name       The name of the effect.
+		 * @param baseEffect The effect itself.
+		 */
+		public Builder(String name, StatusEffect baseEffect) {
+			this.name = name;
+			this.baseEffect = baseEffect;
+		}
+
+		public EffectCreator build() {
+			return new EffectCreator(this.name, this.baseEffect);
+		}
+	}
+}
