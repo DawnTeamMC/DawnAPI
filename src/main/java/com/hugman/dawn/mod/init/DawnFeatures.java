8717f18284384bf591c5921e57abe7d7a8eed69f
Hugman <contact.hugman@gmail.com>
2020-10-31 13:01:15 +0100

    Deprecated warning

--- src/main/java/com/hugman/dawn/mod/init/DawnFeatures.java
+++ src/main/java/com/hugman/dawn/mod/init/DawnFeatures.java
@@ -5,5 +5,9 @@ import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.TreeFeatureConfig;
 
 public class DawnFeatures {
+	/**
+	 * @deprecated Use the terraform API instead. Will be removed in v1.6.
+	 */
+	@Deprecated
 	public static final Feature<TreeFeatureConfig> SANDY_TREE = TerraformTreeFeatures.SANDY_TREE;
 }
