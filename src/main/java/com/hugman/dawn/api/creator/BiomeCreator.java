9fa4212bb64a67e22bd760f758b67ab48ce02b18
Hugman <contact.hugman@gmail.com>
2020-11-23 16:03:16 +0100

    Bump version

--- src/main/java/com/hugman/dawn/api/creator/BiomeCreator.java
+++ src/main/java/com/hugman/dawn/api/creator/BiomeCreator.java
@@ -1,9 +1,6 @@
 package com.hugman.dawn.api.creator;
 
 import com.hugman.dawn.api.util.ModData;
-import net.fabricmc.fabric.api.biome.v1.NetherBiomes;
-import net.fabricmc.fabric.api.biome.v1.OverworldBiomes;
-import net.fabricmc.fabric.api.biome.v1.OverworldClimate;
 import net.minecraft.util.registry.BuiltinRegistries;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.RegistryKey;
@@ -11,55 +8,20 @@ import net.minecraft.world.biome.Biome;
 
 public class BiomeCreator extends Creator<RegistryKey<Biome>> {
 	private final Biome biome;
-	private final SpawnDimension spawnDimension;
 
-	private final OverworldClimate climate;
-	private final double weight;
-
-	private final Biome.MixedNoisePoint noises;
-
-	private BiomeCreator(ModData modData, String name, Biome biome, SpawnDimension spawnDimension, OverworldClimate climate, double weight, Biome.MixedNoisePoint noises) {
+	private BiomeCreator(ModData modData, String name, Biome biome) {
 		super(modData, name, RegistryKey.of(Registry.BIOME_KEY, modData.id(name)));
 		this.biome = biome;
-		this.spawnDimension = spawnDimension;
-		this.climate = climate;
-		this.weight = weight;
-		this.noises = noises;
 	}
 
 	@Override
 	public void register() {
 		Registry.register(BuiltinRegistries.BIOME, value.getValue(), biome);
-		switch(this.spawnDimension) {
-			case NONE:
-			default:
-				break;
-			case OVERWORLD_CONTINENTAL:
-				OverworldBiomes.addContinentalBiome(value, climate, weight);
-				break;
-			case THE_NETHER:
-				NetherBiomes.addNetherBiome(value, noises);
-				break;
-		}
-	}
-
-	@Deprecated
-	public enum SpawnDimension {
-		NONE,
-		OVERWORLD_CONTINENTAL,
-		THE_NETHER,
-		THE_END
 	}
 
 	public static class Builder implements Creator.Builder<RegistryKey<Biome>> {
 		private final String name;
 		private final Biome biome;
-		private SpawnDimension spawnDimension;
-
-		private OverworldClimate climate;
-		private double weight;
-
-		private Biome.MixedNoisePoint noises;
 
 		/**
 		 * Creates a biome.
@@ -70,38 +32,10 @@ public class BiomeCreator extends Creator<RegistryKey<Biome>> {
 		public Builder(String name, Biome biome) {
 			this.name = name;
 			this.biome = biome;
-			this.spawnDimension = SpawnDimension.NONE;
-		}
-
-		/**
-		 * Adds the biome to the overworld continental generation.
-		 *
-		 * @param climate The biome climate.
-		 * @param weight  The biome weight.
-		 * @deprecated Use {@link OverworldBiomes#addContinentalBiome} instead. Method will be removed next update.
-		 */
-		@Deprecated
-		public Builder addToOverworldContinental(OverworldClimate climate, double weight) {
-			this.spawnDimension = SpawnDimension.OVERWORLD_CONTINENTAL;
-			this.climate = climate;
-			this.weight = weight;
-			return this;
-		}
-
-		/**
-		 * Adds the biome to the nether generation.
-		 *
-		 * @deprecated Use {@link OverworldBiomes#addContinentalBiome} instead. Method will be removed next update.
-		 */
-		@Deprecated
-		public Builder addToNether(float temperature, float humidity, float altitude, float weirdness, float weight) {
-			this.spawnDimension = SpawnDimension.THE_NETHER;
-			this.noises = new Biome.MixedNoisePoint(temperature, humidity, altitude, weirdness, weight);
-			return this;
 		}
 
 		public BiomeCreator build(ModData modData) {
-			return new BiomeCreator(modData, this.name, this.biome, this.spawnDimension, this.climate, this.weight, this.noises);
+			return new BiomeCreator(modData, this.name, this.biome);
 		}
 	}
 }
