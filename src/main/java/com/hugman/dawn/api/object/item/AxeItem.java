ab6794a377721396e668b3c356191536274fecb7
Hugman <contact.hugman@gmail.com>
2021-03-31 15:12:47 +0200

    Removed all effects and enchantments
    - Added enchantments and entity types information to the debug list
    - Ported 4 effects to CE:F and deleted the others
    - Removed enchantments

--- src/main/java/com/hugman/dawn/api/object/item/AxeItem.java
+++ src/main/java/com/hugman/dawn/api/object/item/AxeItem.java
@@ -5,7 +5,7 @@ import net.minecraft.block.Block;
 import net.minecraft.item.ToolMaterial;
 
 public class AxeItem extends net.minecraft.item.AxeItem {
-	public static final Builder<Block, Block> BLOCK_STRIPPING_MAP = new Builder<Block, Block>().putAll(STRIPPED_BLOCKS);
+	public static final Builder<Block, Block> BLOCK_STRIPPING_MAP = new Builder<>();
 
 	/* Extension for internal publicity */
 	public AxeItem(ToolMaterial material, float attackDamage, float attackSpeed, Settings settings) {
