184dbf3509ef2399d0b6ec0195b4efa8093e6469
Hugman <contact.hugman@gmail.com>
2021-04-12 22:24:03 +0200

    Fixed my dumb 3

--- src/main/java/com/hugman/dawn/api/creator/FeatureCreator.java
+++ src/main/java/com/hugman/dawn/api/creator/FeatureCreator.java
@@ -1,12 +1,11 @@
 package com.hugman.dawn.api.creator;
 
-import com.hugman.dawn.api.object.ModData;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.FeatureConfig;
 
 public class FeatureCreator<FC extends FeatureConfig, F extends Feature<FC>> extends SimpleCreator<F> {
-	public FeatureCreator(ModData modData, String name, F feature) {
+	public FeatureCreator(String name, F feature) {
 		super(Registry.FEATURE, name, feature);
 	}
 }
