20b78cd7d88c1b93d5f820cb936baea85c8e698d
Hugman <contact.hugman@gmail.com>
2021-04-13 13:14:33 +0200

    bruh

--- src/main/java/com/hugman/dawn/api/creator/SoundCreator.java
+++ src/main/java/com/hugman/dawn/api/creator/SoundCreator.java
@@ -1,10 +1,24 @@
 package com.hugman.dawn.api.creator;
 
+import com.hugman.dawn.api.object.ModData;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.registry.Registry;
 
-public class SoundCreator extends SimpleCreator<SoundEvent> {
+public class SoundCreator extends Creator {
+	private final String name;
+	private SoundEvent sound;
+
 	public SoundCreator(String name) {
-		super(Registry.SOUND_EVENT, name, null);
+		this.name = name;
+	}
+
+	public SoundEvent getSound() {
+		return sound;
+	}
+
+	@Override
+	public void register(ModData modData) {
+		this.sound = new SoundEvent(modData.id(this.name));
+		Registry.register(Registry.SOUND_EVENT, modData.id(this.name), new SoundEvent(modData.id(this.name)));
 	}
 }
