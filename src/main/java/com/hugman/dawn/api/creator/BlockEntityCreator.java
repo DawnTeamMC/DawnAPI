69f74d10bae1e13a422010f6e32f7bc891c1204e
Hugman <contact.hugman@gmail.com>
2021-06-12 12:45:48 +0200

    Fixed an issue with BlockEntityCreator

--- src/main/java/com/hugman/dawn/api/creator/BlockEntityCreator.java
+++ src/main/java/com/hugman/dawn/api/creator/BlockEntityCreator.java
@@ -1,5 +1,6 @@
 package com.hugman.dawn.api.creator;
 
+import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityTypeBuilder;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.datafixer.TypeReferences;
@@ -7,7 +8,7 @@ import net.minecraft.util.Util;
 import net.minecraft.util.registry.Registry;
 
 public class BlockEntityCreator<E extends BlockEntity> extends SimpleCreator<BlockEntityType<E>> {
-	public BlockEntityCreator(String name, BlockEntityType.Builder<E> builder) {
+	public BlockEntityCreator(String name, FabricBlockEntityTypeBuilder<E> builder) {
 		super(Registry.BLOCK_ENTITY_TYPE, name, builder.build(Util.getChoiceType(TypeReferences.BLOCK_ENTITY, name)));
 	}
 }
