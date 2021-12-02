bcd65d26f3f550e5205483277d9ad1417de91d37
Hugman <contact.hugman@gmail.com>
2021-12-02 16:47:45 +0100

    Improved StructurePieceCreator

--- src/main/java/com/hugman/dawn/api/creator/StructurePieceCreator.java
+++ src/main/java/com/hugman/dawn/api/creator/StructurePieceCreator.java
@@ -7,13 +7,33 @@ import net.minecraft.util.registry.Registry;
  * A class allowing a structure piece type to be created.
  */
 public class StructurePieceCreator extends SimpleCreator<StructurePieceType> {
-
 	/**
 	 * Creates a structure piece type.
-	 * @param name the name of the structure piece type
+	 *
+	 * @param name  the name of the structure piece type
 	 * @param piece the structure piece type itself
 	 */
 	public StructurePieceCreator(String name, StructurePieceType piece) {
 		super(Registry.STRUCTURE_PIECE, name, piece);
+	}
+
+	/**
+	 * Creates a structure piece type.
+	 *
+	 * @param name  the name of the structure piece type
+	 * @param piece the structure piece type itself
+	 */
+	public StructurePieceCreator(String name, StructurePieceType.Simple piece) {
+		this(name, (StructurePieceType) piece);
+	}
+
+	/**
+	 * Creates a structure piece type.
+	 *
+	 * @param name  the name of the structure piece type
+	 * @param piece the structure piece type itself
+	 */
+	public StructurePieceCreator(String name, StructurePieceType.ManagerAware piece) {
+		this(name, (StructurePieceType) piece);
 	}
 }
