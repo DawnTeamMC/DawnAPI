f4f8bcea2fdaf38f21d81fa52e91a10ecd2757b1
Hugman <contact.hugman@gmail.com>
2021-12-19 19:26:26 +0100

    Some bug fixing again lol

--- src/main/java/com/hugman/dawn/api/util/DefaultBlockBuilders.java
+++ src/main/java/com/hugman/dawn/api/util/DefaultBlockBuilders.java
@@ -8,15 +8,11 @@ import com.hugman.dawn.api.object.block.StairsBlock;
 import com.hugman.dawn.api.object.block.StoneButtonBlock;
 import com.hugman.dawn.api.object.block.TrapdoorBlock;
 import com.hugman.dawn.api.object.block.WoodButtonBlock;
-import net.minecraft.block.Block;
-import net.minecraft.block.FenceBlock;
-import net.minecraft.block.FenceGateBlock;
-import net.minecraft.block.LeavesBlock;
-import net.minecraft.block.MushroomBlock;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.*;
 import net.minecraft.block.PressurePlateBlock.ActivationRule;
-import net.minecraft.block.SlabBlock;
-import net.minecraft.block.WallBlock;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.sound.BlockSoundGroup;
 
 public class DefaultBlockBuilders {
 	public static final BlockCreator.Builder CUBE = new BlockCreator.Builder().provider(Block::new).itemGroup(ItemGroup.BUILDING_BLOCKS);
@@ -24,16 +20,16 @@ public class DefaultBlockBuilders {
 	public static final BlockCreator.Builder STAIRS = new BlockCreator.Builder().provider(StairsBlock::new).itemGroup(ItemGroup.BUILDING_BLOCKS);
 	public static final BlockCreator.Builder SLAB = new BlockCreator.Builder().provider(SlabBlock::new).itemGroup(ItemGroup.BUILDING_BLOCKS);
 	public static final BlockCreator.Builder WALL = new BlockCreator.Builder().provider(WallBlock::new).itemGroup(ItemGroup.DECORATIONS);
-	public static final BlockCreator.Builder STONE_BUTTON = new BlockCreator.Builder().provider(s -> new StoneButtonBlock(s.noCollision().strength(0.5F))).itemGroup(ItemGroup.REDSTONE);
-	public static final BlockCreator.Builder WOOD_BUTTON = new BlockCreator.Builder().provider(s -> new WoodButtonBlock(s.noCollision().strength(0.5F))).itemGroup(ItemGroup.REDSTONE);
-	public static final BlockCreator.Builder STONE_PRESSURE_PLATE = new BlockCreator.Builder().provider(s -> new PressurePlateBlock(ActivationRule.MOBS, s.requiresTool().noCollision().strength(0.5F))).itemGroup(ItemGroup.REDSTONE);
-	public static final BlockCreator.Builder WOOD_PRESSURE_PLATE = new BlockCreator.Builder().provider(s -> new PressurePlateBlock(ActivationRule.EVERYTHING, s.noCollision().strength(0.5F))).itemGroup(ItemGroup.REDSTONE);
+	public static final BlockCreator.Builder STONE_BUTTON = new BlockCreator.Builder().provider(s -> new StoneButtonBlock(s.noCollision().strength(0.5F))).settings(FabricBlockSettings.of(Material.DECORATION).noCollision().hardness(0.5F)).itemGroup(ItemGroup.REDSTONE);
+	public static final BlockCreator.Builder WOOD_BUTTON = new BlockCreator.Builder().provider(s -> new WoodButtonBlock(s.noCollision().strength(0.5F))).settings(FabricBlockSettings.of(Material.DECORATION).noCollision().hardness(0.5F).sounds(BlockSoundGroup.WOOD)).itemGroup(ItemGroup.REDSTONE);
+	public static final BlockCreator.Builder STONE_PRESSURE_PLATE = new BlockCreator.Builder().provider(s -> new PressurePlateBlock(ActivationRule.MOBS, s.requiresTool().noCollision().strength(0.5F))).settings(FabricBlockSettings.of(Material.STONE).requiresTool().noCollision().strength(0.5F)).itemGroup(ItemGroup.REDSTONE);
+	public static final BlockCreator.Builder WOOD_PRESSURE_PLATE = new BlockCreator.Builder().provider(s -> new PressurePlateBlock(ActivationRule.EVERYTHING, s.noCollision().strength(0.5F))).settings(FabricBlockSettings.of(Material.WOOD).noCollision().strength(0.5F).sounds(BlockSoundGroup.WOOD)).itemGroup(ItemGroup.REDSTONE);
 	public static final BlockCreator.Builder TRAPDOOR = new BlockCreator.Builder().provider(s -> new TrapdoorBlock(s.strength(3.0F).nonOpaque().allowsSpawning(DefaultBlockSettings::never))).itemGroup(ItemGroup.REDSTONE).render(BlockCreator.Render.CUTOUT);
 	public static final BlockCreator.Builder DOOR = new BlockCreator.Builder().provider(s -> new DoorBlock(s.strength(3.0F).nonOpaque())).itemGroup(ItemGroup.REDSTONE).render(BlockCreator.Render.CUTOUT);
 	public static final BlockCreator.Builder FENCE = new BlockCreator.Builder().provider(FenceBlock::new).itemGroup(ItemGroup.DECORATIONS);
 	public static final BlockCreator.Builder FENCE_GATE = new BlockCreator.Builder().provider(FenceGateBlock::new).itemGroup(ItemGroup.REDSTONE);
-	public static final BlockCreator.Builder LEAVES = new BlockCreator.Builder().provider(LeavesBlock::new).itemGroup(ItemGroup.DECORATIONS).render(BlockCreator.Render.CUTOUT_MIPPED).flammability(30, 60).compostingChance(0.3f);
-	public static final BlockCreator.Builder SAPLING = new BlockCreator.Builder().settings(DefaultBlockSettings.SAPLING).compostingChance(0.3f).itemGroup(ItemGroup.DECORATIONS).render(BlockCreator.Render.CUTOUT_MIPPED);
-	public static final BlockCreator.Builder MUSHROOM_BLOCK = new BlockCreator.Builder().provider(MushroomBlock::new).itemGroup(ItemGroup.DECORATIONS).compostingChance(0.85F);
-	public static final BlockCreator.Builder MUSHROOM = new BlockCreator.Builder().provider(MushroomPlantBlock::new).itemGroup(ItemGroup.DECORATIONS).render(BlockCreator.Render.CUTOUT).compostingChance(0.65F);
+	public static final BlockCreator.Builder LEAVES = new BlockCreator.Builder().settings(FabricBlockSettings.of(Material.LEAVES).strength(0.2F).ticksRandomly().sounds(BlockSoundGroup.GRASS).nonOpaque().allowsSpawning(DefaultBlockSettings::canSpawnOnLeaves).suffocates(DefaultBlockSettings::never).blockVision(DefaultBlockSettings::never)).provider(LeavesBlock::new).itemGroup(ItemGroup.DECORATIONS).render(BlockCreator.Render.CUTOUT_MIPPED).flammability(30, 60).compostingChance(0.3f);
+	public static final BlockCreator.Builder SAPLING = new BlockCreator.Builder().settings(FabricBlockSettings.of(Material.PLANT).sounds(BlockSoundGroup.GRASS).breakInstantly().noCollision().ticksRandomly()).compostingChance(0.3f).itemGroup(ItemGroup.DECORATIONS).render(BlockCreator.Render.CUTOUT_MIPPED);
+	public static final BlockCreator.Builder MUSHROOM_BLOCK = new BlockCreator.Builder().provider(MushroomBlock::new).settings(FabricBlockSettings.of(Material.WOOD).hardness(0.2F).sounds(BlockSoundGroup.WOOD)).itemGroup(ItemGroup.DECORATIONS).compostingChance(0.85F);
+	public static final BlockCreator.Builder MUSHROOM = new BlockCreator.Builder().provider(MushroomPlantBlock::new).settings(FabricBlockSettings.of(Material.PLANT).noCollision().hardness(0.0F).sounds(BlockSoundGroup.GRASS).luminance(1)).itemGroup(ItemGroup.DECORATIONS).render(BlockCreator.Render.CUTOUT).compostingChance(0.65F);
 }
--- src/main/java/com/hugman/dawn/api/util/DefaultBlockSettings.java
+++ src/main/java/com/hugman/dawn/api/util/DefaultBlockSettings.java
@@ -12,19 +12,8 @@ public class DefaultBlockSettings {
 	public static final FabricBlockSettings LOG = FabricBlockSettings.of(Material.WOOD).strength(2.0F).sounds(BlockSoundGroup.WOOD);
 	public static final FabricBlockSettings STEM = FabricBlockSettings.of(Material.NETHER_WOOD).strength(2.0F).sounds(BlockSoundGroup.NETHER_STEM);
 
-	public static final FabricBlockSettings LEAVES = FabricBlockSettings.of(Material.LEAVES).strength(0.2F).ticksRandomly().sounds(BlockSoundGroup.GRASS).nonOpaque().allowsSpawning(DefaultBlockSettings::canSpawnOnLeaves).suffocates(DefaultBlockSettings::never).blockVision(DefaultBlockSettings::never);
-	public static final FabricBlockSettings SAPLING = FabricBlockSettings.of(Material.PLANT).sounds(BlockSoundGroup.GRASS).breakInstantly().noCollision().ticksRandomly();
 	public static final FabricBlockSettings FUNGUS = FabricBlockSettings.of(Material.PLANT).sounds(BlockSoundGroup.FUNGUS).breakInstantly().noCollision();
 	public static final FabricBlockSettings POTTED_PLANT = FabricBlockSettings.of(Material.DECORATION).breakInstantly().nonOpaque();
-
-	public static final FabricBlockSettings STONE_PRESSURE_PLATE = FabricBlockSettings.of(Material.STONE).requiresTool().noCollision().strength(0.5F);
-	public static final FabricBlockSettings WOOD_PRESSURE_PLATE = FabricBlockSettings.of(Material.WOOD).noCollision().strength(0.5F).sounds(BlockSoundGroup.WOOD);
-
-	public static final FabricBlockSettings STONE_BUTTON = FabricBlockSettings.of(Material.DECORATION).noCollision().hardness(0.5F);
-	public static final FabricBlockSettings WOOD_BUTTON = FabricBlockSettings.of(Material.DECORATION).noCollision().hardness(0.5F).sounds(BlockSoundGroup.WOOD);
-
-	public static final FabricBlockSettings MUSHROOM_BLOCK = FabricBlockSettings.of(Material.WOOD).hardness(0.2F).sounds(BlockSoundGroup.WOOD);
-	public static final FabricBlockSettings MUSHROOM = FabricBlockSettings.of(Material.PLANT).noCollision().hardness(0.0F).sounds(BlockSoundGroup.GRASS).lightLevel(1);
 
 	public static boolean canSpawnOnLeaves(BlockState state, BlockView world, BlockPos pos, EntityType<?> type) {
 		return type == EntityType.OCELOT || type == EntityType.PARROT;
