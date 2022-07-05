46a7fdc259c80375ee8835bc05f61f837e462eeb
Hugman <contact.hugman@gmail.com>
2022-07-05 02:30:29 +0200

    Added empty shape type

--- src/main/java/com/hugman/dawn/api/init/shape/ShapeType.java
+++ src/main/java/com/hugman/dawn/api/init/shape/ShapeType.java
@@ -7,6 +7,8 @@ import com.mojang.serialization.Codec;
 import net.minecraft.util.registry.Registry;
 
 public record ShapeType<P extends Shape>(Codec<P> codec) {
+	public static final ShapeType<EmptyShape> EMPTY = register("empty", EmptyShape.CODEC);
+	
 	public static final ShapeType<RectangleShape> RECTANGLE = register("rectangle", RectangleShape.CODEC);
 	public static final ShapeType<RectangularPrismShape> RECTANGULAR_PRISM = register("rectangular_prism", RectangularPrismShape.CODEC);
 	public static final ShapeType<RectangularPyramidShape> RECTANGULAR_PYRAMID = register("rectangular_pyramid", RectangularPyramidShape.CODEC);
