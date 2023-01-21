98f5ac0941c17bf65415a079faaa8bb46d8aa13a
Hugo Landrin <contact.hugman@gmail.com>
2023-01-21 11:32:19 +0100

    Fix shape field in shape worldgen feature config

--- src/main/java/fr/hugman/dawn/world/gen/feature/ShapeFeatureConfig.java
+++ src/main/java/fr/hugman/dawn/world/gen/feature/ShapeFeatureConfig.java
@@ -11,7 +11,7 @@ import net.minecraft.world.gen.stateprovider.BlockStateProvider;
 
 public record ShapeFeatureConfig(ConfiguredShape shape, BlockStateProvider state, FloatProvider yOffset) implements FeatureConfig {
 	public static final Codec<ShapeFeatureConfig> CODEC = RecordCodecBuilder.create((instance) -> instance.group(
-			DawnRegistries.CONFIGURED_SHAPE.getEntryCodec().fieldOf("processors").forGetter((config) -> config.shape),
+			DawnRegistries.CONFIGURED_SHAPE.getEntryCodec().fieldOf("shape").forGetter((config) -> config.shape),
 			BlockStateProvider.TYPE_CODEC.fieldOf("state").forGetter((config) -> config.state),
 			FloatProvider.VALUE_CODEC.fieldOf("y_offset").orElse(ConstantFloatProvider.create(0.0F)).forGetter((config) -> config.yOffset)
 	).apply(instance, ShapeFeatureConfig::new));
